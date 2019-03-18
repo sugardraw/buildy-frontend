abb3e4be28d22547eb090e159e5f82c46b45617f
Gaia Cicaloni <39329527+GaiaDCI@users.noreply.github.com>
2019-03-18 14:46:59 +0100

    Login Structure
    first structure Monday 18

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableHighlight, Image, Alert } from 'react-native';
+import React from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Image,
+	Alert,
+	TouchableHighlight
+} from 'react-native';
 
-export default class LoginView extends Component {
+export default class Login extends React.Component {
+
 	static navigationOptions = {
-		title : 'Login'
+		title: 'Login'
 	};
+
 	constructor(props) {
 		super(props);
 		state = {
-			email    : '',
-			password : ''
+			email: '',
+			password: ''
 		};
 	}
 
 	onClickListener = (viewId) => {
-		Alert.alert('Alert', 'Button pressed ' + viewId);
+		Alert.alert('Alert', 'Button pressed' + viewId);
 	};
 
 	render() {
 		return (
 			<View style={styles.container}>
-				<View style={styles.inputContainer}>
+				<Text>LOGIN</Text>
+				<View style={styles.InputContainer}>
 					<Image
 						style={styles.inputIcon}
 						source={{ uri: 'https://png.icons8.com/message/ultraviolet/50/3498db' }}
@@ -30,11 +41,11 @@ export default class LoginView extends Component {
 						placeholder="Email"
 						keyboardType="email-address"
 						underlineColorAndroid="transparent"
-						onChangeText={(email) => this.setState({ email })}
+						onChangeText={(email => this.setState({ email }))}
 					/>
 				</View>
 
-				<View style={styles.inputContainer}>
+				<View style={styles.InputContainer}>
 					<Image
 						style={styles.inputIcon}
 						source={{ uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db' }}
@@ -44,75 +55,83 @@ export default class LoginView extends Component {
 						placeholder="Password"
 						secureTextEntry={true}
 						underlineColorAndroid="transparent"
-						onChangeText={(password) => this.setState({ password })}
+						onChangeText={(password => this.setState({ password }))}
 					/>
 				</View>
 
 				<TouchableHighlight
-					style={[ styles.buttonContainer, styles.loginButton ]}
+					style={[styles.buttonContainer, styles.loginButton]}
 					onPress={() => this.onClickListener('login')}
 				>
 					<Text style={styles.loginText}>Login</Text>
 				</TouchableHighlight>
 
 				<TouchableHighlight
-					style={styles.buttonContainer}
+					style={[styles.buttonContainer]}
 					onPress={() => this.onClickListener('restore_password')}
 				>
-					<Text>Forgot your password?</Text>
+					<Text style={styles.signupSmallLink}>Forgot your password?</Text>
 				</TouchableHighlight>
 
-				<TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
-					<Text>Register</Text>
+				<TouchableHighlight
+					style={[styles.buttonContainer]}
+					onPress={() => this.onClickListener('register')}
+				>
+					<Text style={styles.signupSmallLink}>Register</Text>
 				</TouchableHighlight>
+
 			</View>
-		);
+		)
 	}
 }
 
 const styles = StyleSheet.create({
-	container       : {
-		flex            : 1,
-		justifyContent  : 'center',
-		alignItems      : 'center',
-		backgroundColor : '#DCDCDC'
+	container: {
+		margin: 20,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'orange'
 	},
-	inputContainer  : {
-		borderBottomColor : '#F5FCFF',
-		backgroundColor   : '#FFFFFF',
-		borderRadius      : 30,
-		borderBottomWidth : 1,
-		width             : 250,
-		height            : 45,
-		marginBottom      : 20,
-		flexDirection     : 'row',
-		alignItems        : 'center'
+	InputContainer: {
+		borderBottomColor: '#F5FCFF',
+		backgroundColor: '#FFFFFF',
+		borderRadius: 30,
+		borderBottomWidth: 1,
+		width: 250,
+		height: 45,
+		margin: 20,
+		alignItems: 'center',
+		flexDirection: 'row'
 	},
-	inputs          : {
-		height            : 45,
-		marginLeft        : 16,
-		borderBottomColor : '#FFFFFF',
-		flex              : 1
+	inputs: {
+		flex: 1,
+		height: 45,
+		marginLeft: 16,
+		borderBottomColor: '#FFFFFF'
 	},
-	inputIcon       : {
-		width          : 30,
-		height         : 30,
-		marginLeft     : 15,
-		justifyContent : 'center'
+	inputIcon: {
+		width: 30,
+		height: 30,
+		marginLeft: 15,
+		justifyContent: 'center'
 	},
-	buttonContainer : {
-		height         : 45,
-		flexDirection  : 'row',
-		justifyContent : 'center',
-		alignItems     : 'center',
-		marginBottom   : 20,
-		width          : 250,
-		borderRadius   : 30
+	buttonContainer: {
+		width: 250,
+		height: 45,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginBottom: 20,
+		borderRadius: 30
 	},
-	loginButton     : {
-		backgroundColor : '#00b5ec'
+	loginButton: {
+		backgroundColor: '#00b5ec'
 	},
-	loginText       : {
-		color : 'white'
+	loginText: {
+		color: 'white'
+	},
+	signupSmallLink: {
+		fontSize: 11,
+		fontWeight: '500'
 	}
 });
