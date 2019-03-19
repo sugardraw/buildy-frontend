8856d6b8a02e52dba4678366f1c5dcaaa732cf82
sergio <sergiousl@yahoo.com>
2019-03-19 14:51:22 +0100

    setting up camera.start.sergio

---
+++ components/ImgPicker/ImgPicker.js
@@ -0,0 +1,113 @@
+const Expo = require("expo");
+
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  CameraRoll
+} from "react-native";
+
+export default class ImgPicker extends React.Component {
+  state = {
+    imgUri: null,
+    topText: "",
+    bottomText: "",
+    hasCameraPermission: false
+  };
+
+  async componentDidMount() {
+    const { status } = await Expo.Permissions.askAsync(
+      Expo.Permissions.CAMERA,
+      Expo.Permissions.CAMERA_ROLL
+    );
+
+    if (status === "granted") {
+      this.setState({
+        hasCameraPermission: true
+      });
+    }
+  }
+
+  render() {
+    if (this.state.hasCameraPermission !== true) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.text}>Image Picker</Text>
+          <Image
+            ref={ref => (this.imageView = ref)}
+            style={{ width: 300, height: 300, backgroundColor: "#dddddd" }}
+            source={{ uri: this.state.imgUri }}
+          />
+
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity style={styles.button} onPress={this._onChoosePic}>
+              <Text style={styles.buttonText}>Choose</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={this._onTakePic}>
+              <Text style={styles.buttonText}>Take</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={this._onSave}>
+              <Text style={styles.buttonText}>Save</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
+    }
+  }
+
+  // When "Choose" is pressed, we show the user's image library
+  // so they may show a photo from disk inside the image view.
+  _onChoosePic = async () => {
+    const { cancelled, uri } = await Expo.ImagePicker.launchImageLibraryAsync();
+    if (!cancelled) {
+      this.setState({ imageUri: uri });
+      // console.log(uri) // this logs correctly
+      // TODO: why isn't this showing up inside the Image on screen?
+    }
+  };
+
+  // When "Take" is pressed, we show the user's camera so they
+  // can take a photo to show inside the image view on screen.
+  _onTakePic = async () => {
+    const { cancelled, uri } = await Expo.ImagePicker.launchCameraAsync({});
+    if (!cancelled) {
+      this.setState({ imgUri: uri });
+    }
+  };
+
+  // When "Save" is pressed, we snapshot whatever is shown inside
+  // of "this.imageView" and save it to the device's camera roll.
+  _onSave = async () => {
+    const uri = await Expo.takeSnapshotAsync(this.imageView, {});
+    await CameraRoll.saveToCameraRoll(uri);
+    window.alert("image saved");
+  };
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 28,
+    margin: 20
+  },
+  buttonText: {
+    fontSize: 21
+  },
+  button: {
+    padding: 13,
+    margin: 15,
+    backgroundColor: "#dddddd"
+  },
+  container: {
+    marginTop: 40,
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center"
+  }
+});
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,60 +1,85 @@
-import _ from 'lodash';
-import React from 'react';
-import { Ionicons } from 'react-native-vector-icons';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-import Posts from '../components/Posts/Posts';
-import Welcome from '../components/Welcome/Welcome';
-import Login from '../components/Login/Login';
+import _ from "lodash";
+import React from "react";
+import { Ionicons } from "react-native-vector-icons";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+import Posts from "../components/Posts/Posts";
+import Welcome from "../components/Welcome/Welcome";
+import Login from "../components/Login/Login";
+import ImgPicker from "../components/ImgPicker/ImgPicker";
 
-const ACTIVE_TAB_COLOR = '#69A6F7';
-const INACTIVE_TAB_COLOR = '#aaa';
+const ACTIVE_TAB_COLOR = "#69A6F7";
+const INACTIVE_TAB_COLOR = "#aaa";
 
 const Icon = ({ name, focused }) => (
-	<Ionicons name={name} size={30} color={focused ? ACTIVE_TAB_COLOR : INACTIVE_TAB_COLOR} />
+  <Ionicons
+    name={name}
+    size={30}
+    color={focused ? ACTIVE_TAB_COLOR : INACTIVE_TAB_COLOR}
+  />
 );
 
 const LoginStack = createStackNavigator({
-	screen : Login
+  screen: Login
 });
 
 LoginStack.navigationOptions = {
-	tabBarLabel : 'Login',
-	tabBarIcon  : ({ focused }) => <Icon name="ios-contact" focused={focused} />
+  tabBarLabel: "Login",
+  tabBarIcon: ({ focused }) => <Icon name="ios-contact" focused={focused} />
+};
+
+/**
+ *
+ *  create a test camera functionality
+ *
+ *
+ *  */
+
+const ImgPickerStack = createStackNavigator({
+  screen: ImgPicker
+});
+
+ImgPickerStack.navigationOptions = {
+  tabBarLabel: "camera",
+  tabBarIcon: ({ focused }) => <Icon name="ios-camera" focused={focused} />
 };
 
 const HomeStack = createStackNavigator({
-	Welcome : Welcome,
-	Posts   : Posts
+  Welcome: Welcome,
+  Posts: Posts
 });
 
 HomeStack.navigationOptions = {
-	tabBarLabel : 'Posts',
-	tabBarIcon  : ({ focused }) => <Icon name="ios-home" focused={focused} />
+  tabBarLabel: "Posts",
+  tabBarIcon: ({ focused }) => <Icon name="ios-home" focused={focused} />
 };
 
 export default createBottomTabNavigator(
-	{
-		HomeStack,
-		LoginStack
-	},
-	{
-		tabBarPosition   : 'bottom',
-		swipeEnabled     : false,
-		animationEnabled : false,
-		tabBarOptions    : {
-			activeTintColor   : ACTIVE_TAB_COLOR,
-			inactiveTintColor : INACTIVE_TAB_COLOR,
-			showLabel         : true,
-			style             : {
-				borderTopWidth : 0,
-				paddingTop     : 3,
-				paddingBottom  : 4,
-				height         : 60,
-				shadowColor    : '#000',
-				shadowOpacity  : 0.1,
-				shadowRadius   : 20,
-				shadowOffset   : { width: 0, height: 0 }
-			}
-		}
-	}
+  {
+    HomeStack,
+    LoginStack,
+    ImgPickerStack
+  },
+  {
+    tabBarPosition: "bottom",
+    swipeEnabled: false,
+    animationEnabled: false,
+    tabBarOptions: {
+      activeTintColor: ACTIVE_TAB_COLOR,
+      inactiveTintColor: INACTIVE_TAB_COLOR,
+      showLabel: true,
+      style: {
+        borderTopWidth: 0,
+        paddingTop: 3,
+        paddingBottom: 4,
+        height: 60,
+        shadowColor: "#000",
+        shadowOpacity: 0.1,
+        shadowRadius: 20,
+        shadowOffset: { width: 0, height: 0 }
+      }
+    }
+  }
 );
