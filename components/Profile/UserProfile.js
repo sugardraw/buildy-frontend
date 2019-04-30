1ef5a18353afa9c717d07ae9c104db8c6697523e
sergio <sergiousl@yahoo.com>
2019-04-30 11:13:06 +0200

    update user fixed

--- components/Profile/UserProfile.js
+++ components/Profile/UserProfile.js
@@ -14,7 +14,7 @@ const config = require("../../config/config.js");
 import { api } from "../../api/api";
 import JWT from "expo-jwt";
 import { MaterialIcons } from "@expo/vector-icons/";
-import { Divider } from 'react-native-elements';
+import { Divider } from "react-native-elements";
 
 export default class UserProfile extends React.Component {
   static navigationOptions = {
@@ -78,87 +78,87 @@ export default class UserProfile extends React.Component {
           <View style={styles.bodyContentProfile}>
             {this.state._fontLoaded && this.state.user.length > 0
               ? this.state.user.map((user, i) => (
-                <View
-                  key={i}
-                >
-                  <View style={styles.main}>
-                    <Image
-                      source={{ uri: user.avatar }}
-                      style={{
-                        width: 130,
-                        height: 130,
-                        borderRadius: 70,
-                        shadowOffset: { width: 1, height: 1 },
-                        shadowRadius: 2,
-                        shadowOpacity: 0.5,
-                      }}
-                    />
-                    <View style={{ flexDirection: 'row', marginTop: 15 }}>
-                      <Text
-                        style={{ fontFamily: "Roboto-Black", fontSize: 20, marginRight: 2, paddingRight: 2 }}
-                      >
-                        {user.first_name}
-                      </Text>
-                      <Text
-                        style={{ fontFamily: "Roboto-Black", fontSize: 20, marginRight: 2, paddingRight: 2 }}
-                      >
-                        {user.last_name}
-                      </Text>
-                    </View>
-                  </View>
-
-                  <View style={styles.paragraphText}>
-                    <Text style={styles.servicesList}>
-                      Contact
-                        </Text>
-                    <View style={{ flexDirection: 'row' }}>
-                      <Text
-                        style={{ marginRight: 2, paddingRight: 2 }}
-                      >
-                        {user.street}
-                      </Text>
-                      <Text>{user.zip}</Text>
-                      <Text>{user.city}</Text>
-                    </View>
-                    <Text>{user.email}</Text>
-                  </View>
-
-                  <View style={styles.paragraphText}>
-                    <Text style={styles.servicesList}>
-                      Your Requests
-                    </Text>
-                    {this.state.estimations.length > 0 ? (
-                      this.state.estimations.map((estimation, i) => (
-                        <View
-                          key={i}
+                  <View key={i}>
+                    <View style={styles.main}>
+                      <Image
+                        source={{ uri: user.avatar }}
+                        style={{
+                          width: 130,
+                          height: 130,
+                          borderRadius: 70,
+                          shadowOffset: { width: 1, height: 1 },
+                          shadowRadius: 2,
+                          shadowOpacity: 0.5
+                        }}
+                      />
+                      <View style={{ flexDirection: "row", marginTop: 15 }}>
+                        <Text
+                          style={{
+                            fontFamily: "Roboto-Black",
+                            fontSize: 20,
+                            marginRight: 2,
+                            paddingRight: 2
+                          }}
                         >
-                          <Text style={styles.estimationsList}>
-                            Send To:
-                            <Text style={{ fontWeight: "bold" }}>
-                              {" " + estimation.sendTo}
+                          {user.first_name}
+                        </Text>
+                        <Text
+                          style={{
+                            fontFamily: "Roboto-Black",
+                            fontSize: 20,
+                            marginRight: 2,
+                            paddingRight: 2
+                          }}
+                        >
+                          {user.last_name}
+                        </Text>
+                      </View>
+                    </View>
+
+                    <View style={styles.paragraphText}>
+                      <Text style={styles.servicesList}>Contact</Text>
+                      <View style={{ flexDirection: "row" }}>
+                        <Text style={{ marginRight: 2, paddingRight: 2 }}>
+                          {user.street}
+                        </Text>
+                        <Text>{user.zip}</Text>
+                        <Text>{user.city}</Text>
+                      </View>
+                      <Text>{user.email}</Text>
+                    </View>
+
+                    <View style={styles.paragraphText}>
+                      <Text style={styles.servicesList}>Your Requests</Text>
+                      {this.state.estimations.length > 0 ? (
+                        this.state.estimations.map((estimation, i) => (
+                          <View key={i}>
+                            <Text style={styles.estimationsList}>
+                              Send To:
+                              <Text style={{ fontWeight: "bold" }}>
+                                {" " + estimation.sendTo}
+                              </Text>
                             </Text>
-                          </Text>
-                          <Text style={styles.estimationsList}>
-                            Title: {estimation.requestData.title}
-                          </Text>
-                          <Text style={styles.estimationsList}>
-                            Description: {estimation.requestData.description}
-                          </Text>
-                          <Text style={styles.estimationsList}>
-                            Budget: {estimation.requestData.budget}
-                          </Text>
-                          <Text style={styles.estimationsList}>
-                            Start date: {estimation.requestData.date}
-                          </Text>
-                          <Divider style={styles.divider} />
-                        </View>
-                      ))
-                    ) : (
+                            <Text style={styles.estimationsList}>
+                              Title: {estimation.requestData.title}
+                            </Text>
+                            <Text style={styles.estimationsList}>
+                              Description: {estimation.requestData.description}
+                            </Text>
+                            <Text style={styles.estimationsList}>
+                              Budget: {estimation.requestData.budget}
+                            </Text>
+                            <Text style={styles.estimationsList}>
+                              Start date: {estimation.requestData.date}
+                            </Text>
+                            <Divider style={styles.divider} />
+                          </View>
+                        ))
+                      ) : (
                         <Text>No estimations requested</Text>
                       )}
+                    </View>
                   </View>
-                </View>
-              ))
+                ))
               : null}
           </View>
         </ScrollView>
@@ -167,14 +167,12 @@ export default class UserProfile extends React.Component {
           <View style={styles.buttonEdit}>
             <TouchableOpacity
               onPress={() =>
-                this.props.navigation.navigate("EditUserProfile")
+                this.props.navigation.navigate("EditUserProfile", {
+                  changeScreen: this.props.navigation.getParam("changeScreen")
+                })
               }
             >
-              <MaterialIcons
-                name="mode-edit"
-                size={30}
-                color="white"
-              />
+              <MaterialIcons name="mode-edit" size={30} color="white" />
             </TouchableOpacity>
           </View>
         </View>
@@ -194,7 +192,7 @@ const styles = StyleSheet.create({
     margin: 0
   },
   main: {
-    alignItems: 'center'
+    alignItems: "center"
   },
   servicesList: {
     textAlign: "justify",
